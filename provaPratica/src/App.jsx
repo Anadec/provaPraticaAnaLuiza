d401a42395e2ca21750a4e90337fda58b1ac71d0
ANA LUIZA DE CASTRO <ana_l_castro@estudante.sesisenai.org.br>
2024-07-04 09:35:04 -0300

    commit

---
+++ provaPratica/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import Clubs from './components/Clubs'
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Clubs />
+    </>
+  )
+}
+
+export default App
---
+++ provaPratica/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <img src="logoSenai.png" alt="" />
+            <h2>Nome: Ana Luiza de Castro - Turma: 3B</h2>
+        </header>
+    )
+}
